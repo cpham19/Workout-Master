00e84352c40eef5f7ac76e6ed571134af41cc3f9
Calvin Pham <calvinthepham@gmail.com>
2017-08-06 00:20:12 -0700

    Combined Profile with Workout-Master

---
+++ app/src/main/java/com/example/calvin/workout/data/UserContract.java
@@ -0,0 +1,18 @@
+package com.example.calvin.workout.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by leona on 8/4/2017.
+ */
+
+public class UserContract {
+    public static class TABLE_USER implements BaseColumns {
+
+        public static final String TABLE_NAME = "user";
+        public static final String COLUMN_NAME_NAME = "name";
+        public static final String COLUMN_NAME_GENDER = "gender";
+        public static final String COLUMN_NAME_HEIGHT= "height";
+        public static final String COLUMN_NAME_WEIGHT = "weight";
+    }
+}
